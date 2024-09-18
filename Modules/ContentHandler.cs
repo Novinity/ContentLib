c624352125f7d63711eb102c104fba0d0be4ddc7
Novinity <novinity63@gmail.com>
2024-09-18 17:47:36 +1000

    Added custom content api

---
+++ Modules/ContentHandler.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContentLib.Modules;
+
+public class ContentHandler {
+    public static List<ContentEvent> EventList = new List<ContentEvent>();
+
+    public static void RegisterEvent(ContentEvent contentEvent) {
+        Plugin.Logger.LogInfo($"[ContentLib] Registering content event for {contentEvent.GetName()}");
+        EventList.Add(contentEvent);
+    }
+
+    public static ushort GetEventID(string contentEventName) {
+        Plugin.Logger.LogInfo(EventList.Count);
+
+        int foundIndex = EventList.FindIndex(match => match.GetType().Name == contentEventName);
+        if (foundIndex == -1) {
+            for (int index = 0; index < EventList.Count; index++) {
+                Plugin.Logger.LogInfo($"[ContentLib_Debug] {EventList[index].GetType().Name}, {contentEventName}, {EventList[index].GetType().Name == contentEventName}");
+            }
+            Plugin.Logger.LogError($"[ContentLib] GetEventID for {contentEventName} returned -1");
+        }
+
+        return (ushort)(2000 + foundIndex);
+    }
+}
+
+[HarmonyPatch(typeof(ContentEventIDMapper))]
+internal class ContentEventIDMapperPatches {
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(ContentEventIDMapper.GetContentEvent))]
+    public static bool GetContentEventPrefix(ref ushort id, ref ContentEvent __result) {
+        Plugin.Logger.LogDebug($"[ContentLib_Debug] GetContentEvent was called: {id} Normalized: {id - 2000} EventList count: {ContentHandler.EventList.Count}");
+        if (id - 2000 < 0) return true;
+
+        ContentEvent? contentEvent = ContentHandler.EventList[id - 2000];
+        if (contentEvent == null) return true;
+
+        __result = (ContentEvent)Activator.CreateInstance(contentEvent.GetType());
+        return false;
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # ContentLib - The Custom Content API
 ContentLib is a Content Warning API mod that allows for easy addition of custom monsters, items (shop and artifacts), and maps.
 
+# Roadmap
+- Custom Monster API
+- Custom Content API (DONE)
+- Custom Item API
+- Custom Map API
+More may be added in future.
+
 # Wiki
 Coming soon!
